076b22a18a758287b8c892f2e37321e9842603a6
Daniel25TE <llu24001@byui.edu>
2025-01-09 18:41:24 -0700

    week 1 excercise 3 and 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int numerales = int.Parse(Console.ReadLine());
+
+        List<int> numbers = new List<int>();
+        int sum = 0;
+
+        while (numerales != 0)
+        {
+           numbers.Add(numerales);
+           Console.Write("Enter your number: ");
+           numerales = int.Parse(Console.ReadLine());
+        }
+
+        foreach (int number in numbers)
+        {
+            
+            Console.WriteLine(number);
+
+        }
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers [0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        
+        Console.WriteLine($"The larger number is: {max}");
+
     }
 }
